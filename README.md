55e44e9aa756a387151a55696fe8f4358ef785b8
Sameer Segal <sameersegal@microsoft.com>
2023-06-04 08:12:20 +0530

    More robust urls

---
+++ README.md
@@ -0,0 +1,3 @@
+# Investment Data
+
+This repository contains a few webscrapers to capture data from trusted financial sources to build a local personal knowledge base. These websites require paid subscriptions and valid credentials.
--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import os
+from urllib.parse import urlparse
 from dotenv import load_dotenv
 from scrapers import PageContent, SeekingAlphaScraper, MotleyFoolScraper
 load_dotenv()
 
+def get_domain(link: str) -> str:
+    domain = urlparse(link).hostname
+    parts = domain.split('.')
+    if len(parts) > 2:
+        domain = '.'.join(parts[-2:])
+    return domain
 
 def scrape(link: str) -> PageContent:
     scraper = None
-    if link.startswith("https://seekingalpha.com"):
+    domain = get_domain(link)
+    if domain == 'seekingalpha.com':
         scraper = SeekingAlphaScraper(
             os.getenv('SEEKING_ALPHA_USERNAME'), os.getenv('SEEKING_ALPHA_PASSWORD'))
-    elif link.startswith("https://www.fool.com"):
+    elif domain == 'fool.com':
         scraper = MotleyFoolScraper(
             os.getenv('MOTLEY_FOOL_USERNAME'), os.getenv('MOTLEY_FOOL_PASSWORD'))
     else:
@@ -31,7 +39,6 @@ def main():
             print('date:', content.date)
             print('title:', content.title)
             print('body:', content.body)
-            # print('body:', content.body[:150] + '...' + content.body[-50:])
             print("\n\n")
 
 
